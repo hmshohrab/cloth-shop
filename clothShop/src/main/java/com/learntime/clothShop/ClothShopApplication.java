89d196636a6d781746524646cb27e7f51dbb189e
hmshohrab <hmshohrab20@gmail.com>
2023-01-05 09:51:46 +0600

    First initial

---
+++ clothShop/src/main/java/com/learntime/clothShop/ClothShopApplication.java
@@ -0,0 +1,13 @@
+package com.learntime.clothShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothShopApplication.class, args);
+	}
+
+}
---
+++ clothShop/src/test/java/com/learntime/clothShop/ClothShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learntime.clothShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
