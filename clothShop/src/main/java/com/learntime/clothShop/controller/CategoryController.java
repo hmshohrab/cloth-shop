d7b1bed0ba00134c6b9c74443cb56770d097d88b
hmshohrab <hmshohrab20@gmail.com>
2023-01-05 10:10:54 +0600

    CategoryController.java

---
+++ clothShop/src/main/java/com/learntime/clothShop/controller/CategoryController.java
@@ -0,0 +1,20 @@
+package com.learntime.clothShop.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * BISMILLAH HIR RAHMAN NIR RAHIM
+ * Created by Shohrab Hossen on 05,January 2023
+ *
+ * @Company Data Grid Limited
+ * @Address 14/A(new), Dhanmondi R/A, Dhaka 1209.
+ * @Email shohrab.datagridltd@gmail.com
+ */
+@RestController("/category")
+class CategoryController {
+/*
+   @PostMapping("create")
+   ResponseEntity<?> create(){
+
+   }*/
+}
---
+++ clothShop/src/main/java/com/learntime/clothShop/repository/CategoryRepository.java
@@ -0,0 +1,17 @@
+package com.learntime.clothShop.repository;
+
+import com.learntime.clothShop.model.CategoryModel;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * BISMILLAH HIR RAHMAN NIR RAHIM
+ * Created by Shohrab Hossen on 05,January 2023
+ *
+ * @Company Data Grid Limited
+ * @Address 14/A(new), Dhanmondi R/A, Dhaka 1209.
+ * @Email shohrab.datagridltd@gmail.com
+ */
+
+public interface CategoryRepository extends JpaRepository<CategoryModel, Long> {
+}
